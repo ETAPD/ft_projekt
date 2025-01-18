0c9c9dca885ca55f47b0fa64e3e6ef3a6005d166
ETAPD <139513318+ETAPD@users.noreply.github.com>
2025-01-18 12:19:09 +0100

    about stranka

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,72 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-view">
+    <div class="image-container">
+      <img src="../../public/media/img/gamer_font.jpg" alt="Gamer Font" class="img-fluid" />
+    </div>
+
+    <div class="container">
+      <h1>Vitajte na našom fóre venovanom hráčom a herným nadšencom!</h1>
+      <p>
+        Sme komunita ľudí, ktorí milujú hry a veria, že spoločné zdieľanie skúseností, tipov a
+        stratégií môže obohatiť herný zážitok každého z nás.
+      </p>
+
+      <h2>Na našom fóre nájdete priestor na:</h2>
+      <ul>
+        <li>Diskusie o hrách: Podeľte sa o svoje názory, recenzie a odporúčania.</li>
+        <li>Stratégie a tipy: Naučte sa nové techniky a zlepšite svoje herné zručnosti.</li>
+        <li>Podpora: Pomôžte si navzájom s technickými problémami a otázkami ohľadom hier.</li>
+        <li>
+          Novinky a aktualizácie: Buďte informovaní o najnovších herných trendoch a udalostiach.
+        </li>
+      </ul>
+
+      <p>
+        Našou misiou je vytvoriť priateľské a podporujúce prostredie, kde sa každý hráč môže cítiť
+        vítaný a môže prispieť svojimi poznatkami. Nezáleží na tom, či ste začiatočník alebo skúsený
+        hráč, u nás si každý nájde svoje miesto.
+      </p>
+
+      <h2>
+        Pridajte sa k nám a staňte sa súčasťou našej rastúcej komunity. Spolu môžeme zlepšiť náš
+        herný svet a zažiť množstvo zábavy!
+      </h2>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+body {
+  margin-top: 10%;
+}
+
+.image-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+h1,
+h2 {
+  color: #2c3e50;
+}
+
+ul {
+  list-style-type: disc;
+  padding-left: 20px;
+}
+
+ul li {
+  margin-bottom: 10px;
 }
 </style>
