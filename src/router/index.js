89ca91dfb1046ba2bee04aa57e8db73300082b33
ETAPD <139513318+ETAPD@users.noreply.github.com>
2025-01-18 16:51:59 +0100

    uprava index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ArticleView from '@/views/ArticleView.vue'
+import ContactView from '@/views/ContactView.vue'
+import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,17 +11,23 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      props: true,
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      component: AboutView,
     },
     {
       path: '/article:id',
       name: 'article',
       component: ArticleView,
       props: true,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView,
     },
   ],
 })
